5323b0dec90c88ceb6cef34e30091ac64a7ae4a9
asids <trunghieu.ngo612@gmail.com>
2021-06-19 10:50:42 +0700

    add comment api

--- src/main/java/com/example/demo/api/RestEmployeeController.java
+++ src/main/java/com/example/demo/api/RestEmployeeController.java
@@ -26,40 +26,86 @@ public class RestEmployeeController {
     @Autowired
     EmployeeRepository repository;
 
-    @RequestMapping(value = "/employees", method = RequestMethod.POST)
+
+    @RequestMapping(value = "/employees/add", method = RequestMethod.POST)
     public ResponseEntity<Employee> insertData(@RequestBody EmployeeDto dto) {
         Employee value = service.insertData(dto);
         return ResponseEntity.ok().body(value);
     }
+    /*
+        http://localhost:8089/api/employees/add
+        {
+            "code": "000001",
+            "name": "as",
+            "email": "4@gmail.com",
+            "phone": "092826732",
+            "age": 21
+        }
+     */
 
     @RequestMapping(path = "/employees", method = RequestMethod.GET)
     public ResponseEntity<List<EmployeeDto>> getAll() {
         return ResponseEntity.ok().body(service.getAll());
     }
+    /*
+            http://localhost:8089/api/employees
 
-    @RequestMapping(path = "/search", method = RequestMethod.GET)
+     */
+
+    @RequestMapping(path = "/employees/search", method = RequestMethod.GET)
     public ResponseEntity<List<EmployeeDto>> search(@RequestBody EmployeeSeachDto dto) {
         return ResponseEntity.ok().body(service.search(dto));
     }
+    /*
+            http://localhost:8089/api/employees/search
+            {
+                "code": "000001",
+                "name": "as",
+                "email": "4@gmail.com",
+                "phone": "092826732",
+                "age": 21
+            }
+         */
 
-    @RequestMapping(path = "/employees/{code}", method = RequestMethod.GET)
+    @RequestMapping(path = "/employees/search/{code}", method = RequestMethod.GET)
     public ResponseEntity<Employee> getEmployeeCode(@PathVariable String code) {
         Employee employee = repository.findByCode(code);
         return ResponseEntity.ok(employee);
     }
+    /*
+            http://localhost:8089/api/employees/search/000001
+    */
 
-    @RequestMapping(path = "/employees/{code}", method = RequestMethod.PUT)
+    @RequestMapping(path = "/employees/update/{code}", method = RequestMethod.PUT)
     public ResponseEntity<Boolean> update(@RequestBody EmployeeDto dto) {
         return ResponseEntity.ok().body(service.update(dto));
     }
+    /*
+            http://localhost:8089/api/update
+            {
+                "code": "000001",
+                "name": "hieu",
+                "email": "hieu@gmail.com",
+                "phone": "092826732",
+                "age": 19
+            }
+     */
 
-    @RequestMapping(path = "/employees/{code}", method = RequestMethod.DELETE)
+    @RequestMapping(path = "/employees/delete/{code}", method = RequestMethod.DELETE)
     public ResponseEntity<Boolean> delete(@PathVariable String code) {
         return ResponseEntity.ok().body(service.delete(code));
     }
+    /*
+            http://localhost:8089/api/employees/delete/000001
+    */
 
     @RequestMapping(path = "/export/excel", method = RequestMethod.GET)
     public void exportToExcel(HttpServletResponse response) throws IOException {
         service.exportToExcel(response);
     }
+    /*
+        http://localhost:8089/api/export/excel
+
+     */
+
 }
